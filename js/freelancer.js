61ab2600a5521c216b342e1cd0e5343d3ad0950d
karina <kramosq@gmail.com>
2015-12-17 11:52:48 -0500

    Avance

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,4 +1,34 @@
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+//$(".navbar-collapse ul li a").click(function() {
+//    $(".navbar-toggle:visible").click();
+//});
+
+//Click en menu
+$(document).ready(main);
+
+function main(){
+    /*$(".menu a").click(function(){
+        //$(".menu li>a").removeClass("active");
+        $(this).addClass("active");
+        
+    });*/
+    
+    $(".menu li>a:last-child").click(function(){
+        $(".menu li>a").removeClass("active");
+        $(this).addClass("active");
+        $(".banner").hide();
+        $("#portfolio").hide();
+        $(".about").hide();
+        $(".main-content").hide();
+        $("section").hide();
+    });
+    
+    if("width"<=619px){
+        alert("holaaa");
+       $(".main-menu").removeClass(pull-right);
+    }
+    else{
+        $(".main-menu").addClass(pull-right);
+    }
+    
+}
